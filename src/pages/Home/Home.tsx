6bdcc497b44ec8549dc1a362130155341c2fc544
geist9110 <geist9110@naver.com>
2025-04-09 07:57:35 +0900

    feat: footer 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Card from '@/components/Card';
 import styles from './Home.module.css';
 import character from '@/assets/person-with-notebook-and-coffee.svg';
 import testImage from '@/assets/testImage.jpg';
+import Footer from '@/components/Footer';
 
 const cards = [
   {
@@ -43,6 +44,7 @@ function Home() {
       <Header />
       <HeroSection />
       <CardSection />
+      <Footer />
     </div>
   );
 }
