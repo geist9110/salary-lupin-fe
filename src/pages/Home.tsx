df81c525499ca45550be8159b2c2212a272fe095
geist9110 <geist9110@naver.com>
2025-04-09 01:28:29 +0900

    feat: 헤더 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import Header from '@/components/Header';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      Home
+    </div>
+  );
 }
 
 export default Home;
