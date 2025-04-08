d087ec119528ac25c2bb71e36e8d929a3da9d76e
geist9110 <geist9110@naver.com>
2025-04-09 02:10:14 +0900

    feat: header 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
+import styles from './Header.module.css';
 import logo from '@/assets/logo.svg';
 
 function Header() {
   return (
-    <div>
-      <img src={logo} alt="logo" />
-    </div>
+    <header className={styles.header}>
+      <img className={styles.logo} src={logo} alt="logo" />
+    </header>
   );
 }
 
