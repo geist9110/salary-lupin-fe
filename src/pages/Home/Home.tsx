4d513d795624d799c7692ab3e02c88a472b8f037
Lee Hyungi <63250720+geist9110@users.noreply.github.com>
2025-04-09 03:55:45 +0900

    홈페이지의 히어로 섹션 구현 (#3)
    * feat: Pretendard 폰트 추가
    
    * feat: 텍스트 스타일 추가
    
    * feat: 간격에 대한 변수 추가
    
    * feat: 히어로 섹션의 텍스트 구현
    
    * feat: 히어로 섹션 이미지 구현
    
    * refactor: page내의 Home 폴더 추가
    
    * refactor: 간격의 large를 xlarge로 변경 및 20px large속성 추가
    
    * feat: 텍스트의 기본 색상 추가

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,31 @@
+import Header from '@/components/Header';
+import styles from './Home.module.css';
+import character from '@/assets/person-with-notebook-and-coffee.svg';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+    </div>
+  );
+}
+
+function HeroSection() {
+  return (
+    <div className={styles.heroSection}>
+      <div>
+        <h1>우리는 일하지 않기 위해 일을 한다.</h1>
+        <h3 className={styles.subtitle}>여러분의 월급루팡을 도와드립니다.</h3>
+        <div className={styles.description}>
+          <p>집중은 무너지고, 할 일은 없고, 상사는 감시하고.</p>
+          <p>그래서 우리는 무언가 해야만 합니다.</p>
+          <p>이건 일하는 척을 돕는 가장 진지한 플랫폼입니다.</p>
+        </div>
+      </div>
+      <img src={character} alt="character" />
+    </div>
+  );
+}
+
+export default Home;
