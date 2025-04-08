5dec909f948021daa52ff41b167de15fd6d2eea2
geist9110 <geist9110@naver.com>
2025-04-09 05:34:32 +0900

    feat: 홈페이지에 카드 컴포넌트 배치 기능 추가
    테스트용 이미지가 존재하므로 최종 pr에서 삭제 필요

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,48 @@
 import Header from '@/components/Header';
+import Card from '@/components/Card';
 import styles from './Home.module.css';
 import character from '@/assets/person-with-notebook-and-coffee.svg';
+import testImage from '@/assets/testImage.jpg';
+
+const cards = [
+  {
+    id: 'contents-001',
+    thumbnail: testImage,
+    title: '일하는 척',
+    description: '일하는 척이 하고 싶으신가요?',
+  },
+  {
+    id: 'contents-002',
+    thumbnail: testImage,
+    title: '마법의 고민 해결 책',
+    description: '고민을 해결해주는 책',
+  },
+  {
+    id: 'contents-003',
+    thumbnail: testImage,
+    title: '샌드위치 휴가 추천',
+    description: '연가를 사용하기 적합한 날짜 추천',
+  },
+  {
+    id: 'contents-004',
+    thumbnail: testImage,
+    title: '고해성사',
+    description: '여러분들의 잘못을 들어 드립니다.',
+  },
+  {
+    id: 'contents-005',
+    thumbnail: testImage,
+    title: '월급루팡 인증서',
+    description: '당신의 월급 루팡을 인증해드립니다.',
+  },
+];
 
 function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <CardSection />
     </div>
   );
 }
@@ -24,6 +60,16 @@ function HeroSection() {
         </div>
       </div>
       <img src={character} alt="character" />
+    </div>
+  );
+}
+
+function CardSection() {
+  return (
+    <div className={styles.cardSection}>
+      {cards.map((card) => (
+        <Card key={card.id} {...card} />
+      ))}
     </div>
   );
 }
