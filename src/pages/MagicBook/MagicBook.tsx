02e258eb2f77fb3a088b3c8d8e1a9e955f3296cb
geist9110 <geist9110@naver.com>
2025-04-11 16:05:31 +0900

    feat: 책등(book spine) 추가 및 커버의 비율이 깨지는 문제 수정

--- src/pages/MagicBook/MagicBook.tsx
+++ src/pages/MagicBook/MagicBook.tsx
@@ -1,10 +1,14 @@
 import BookCover from '@/assets/book-cover.svg';
+import BookSpine from '@/assets/book-spine.svg';
 import styles from './MagicBook.module.css';
 
 function MagicBook() {
   return (
     <div className={styles.container}>
-      <img src={BookCover} alt="book-cover" />
+      <div className={styles.book}>
+        <img src={BookSpine} alt="book-spine" />
+        <img src={BookCover} alt="book-cover" />
+      </div>
     </div>
   );
 }
