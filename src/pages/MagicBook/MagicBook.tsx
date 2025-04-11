ca2aca44e3854066c817b5d4e5dd411f50df57a2
geist9110 <geist9110@naver.com>
2025-04-11 15:26:17 +0900

    feat: 책 표지 추가

--- src/pages/MagicBook/MagicBook.tsx
+++ src/pages/MagicBook/MagicBook.tsx
@@ -1,5 +1,12 @@
+import BookCover from '@/assets/book-cover.svg';
+import styles from './MagicBook.module.css';
+
 function MagicBook() {
-  return <div>Contents</div>;
+  return (
+    <div className={styles.container}>
+      <img src={BookCover} alt="book-cover" />
+    </div>
+  );
 }
 
 export default MagicBook;
