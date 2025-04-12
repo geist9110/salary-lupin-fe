711253575286ee478944feb1e4ddb76737c14ef3
geist9110 <geist9110@naver.com>
2025-04-12 13:07:38 +0900

    refactor: MagicBook의 page 컴포넌트 분리

--- src/pages/MagicBook/MagicBook.tsx
+++ src/pages/MagicBook/MagicBook.tsx
@@ -2,26 +2,49 @@ import BookCover from '@/assets/book-cover.svg';
 import BookCoverBack from '@/assets/book-cover-back.svg';
 import BookSpine from '@/assets/book-spine.svg';
 import styles from './MagicBook.module.css';
-import { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import applyDraggableFalse from '@/utils/applyDraggableFalse.ts';
 
 function MagicBook() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.book}>
+        <img src={BookSpine} alt="book-spine" draggable={false} className={styles.bookSpine} />
+        <Page
+          front={<img src={BookCover} alt="book-cover" />}
+          back={<img src={BookCoverBack} alt="book-cover-back" />}
+        />
+      </div>
+    </div>
+  );
+}
+
+type PageProps = {
+  front: React.ReactNode;
+  back: React.ReactNode;
+};
+
+function Page({ front, back }: Readonly<PageProps>) {
   const [rotation, setRotation] = useState(0);
+  const pageRef = useRef<HTMLDivElement>(null);
   const startXRef = useRef<number | null>(null);
+  const originXRef = useRef<number | null>(null);
   const isDraggingRef = useRef(false);
 
   const handlePointerDown = (e: React.PointerEvent) => {
     startXRef.current = e.clientX;
+    originXRef.current = pageRef.current?.getBoundingClientRect().left ?? 0;
     isDraggingRef.current = true;
   };
 
-  const handlePointerMove = (e: PointerEvent) => {
-    if (!isDraggingRef.current || startXRef.current === null) return;
-    const deltaX = startXRef.current - e.clientX;
+  const handlePointerMove = useCallback((e: PointerEvent) => {
+    if (!isDraggingRef.current || originXRef.current === null) return;
+    const deltaX = originXRef.current - e.clientX;
     const newRotation = Math.min(Math.max(0, deltaX / 2), 180);
     setRotation(newRotation);
-  };
+  }, []);
 
-  const handlePointerUp = () => {
+  const handlePointerUp = useCallback(() => {
     if (!isDraggingRef.current) return;
     if (rotation > 90) {
       setRotation(180);
@@ -30,10 +53,9 @@ function MagicBook() {
     }
     isDraggingRef.current = false;
     startXRef.current = null;
-  };
+  }, [rotation]);
 
   useEffect(() => {
-    // 전역 이벤트 등록
     window.addEventListener('pointermove', handlePointerMove);
     window.addEventListener('pointerup', handlePointerUp);
 
@@ -41,27 +63,22 @@ function MagicBook() {
       window.removeEventListener('pointermove', handlePointerMove);
       window.removeEventListener('pointerup', handlePointerUp);
     };
-  }, [rotation]);
+  }, [handlePointerMove, handlePointerUp]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.book}>
-        <img src={BookSpine} alt="book-spine" draggable={false} className={styles.bookSpine} />
-        <div
-          className={styles.page}
-          onPointerDown={handlePointerDown}
-          style={{
-            transform: `rotateY(${rotation}deg)`,
-          }}
-        >
-          <img src={BookCover} alt="book-cover" draggable={false} className={styles.pageFront} />
-          <img
-            src={BookCoverBack}
-            alt="book-cover-back"
-            draggable={false}
-            className={styles.pageBack}
-          />
-        </div>
+    <div
+      ref={pageRef}
+      className={styles.page}
+      onPointerDown={handlePointerDown}
+      style={{
+        transform: `rotateY(${rotation}deg)`,
+      }}
+    >
+      <div draggable={false} className={styles.pageFront}>
+        {applyDraggableFalse(front)}
+      </div>
+      <div draggable={false} className={styles.pageBack}>
+        {applyDraggableFalse(back)}
       </div>
     </div>
   );
