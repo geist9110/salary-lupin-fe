79fac3d9e899f54a73fdbdcb6b49d1c67e6225c7
geist9110 <geist9110@naver.com>
2025-04-12 11:01:38 +0900

    feat: 책 커버의 뒷면 추가 및 드래그 기본값 삭제

--- src/pages/MagicBook/MagicBook.tsx
+++ src/pages/MagicBook/MagicBook.tsx
@@ -1,4 +1,5 @@
 import BookCover from '@/assets/book-cover.svg';
+import BookCoverBack from '@/assets/book-cover-back.svg';
 import BookSpine from '@/assets/book-spine.svg';
 import styles from './MagicBook.module.css';
 
@@ -6,8 +7,16 @@ function MagicBook() {
   return (
     <div className={styles.container}>
       <div className={styles.book}>
-        <img src={BookSpine} alt="book-spine" />
-        <img src={BookCover} alt="book-cover" />
+        <img src={BookSpine} alt="book-spine" draggable={false} />
+        <div className={styles.page}>
+          <img src={BookCover} alt="book-cover" draggable={false} />
+          <img
+            src={BookCoverBack}
+            alt="book-cover-back"
+            className={styles.pageBack}
+            draggable={false}
+          />
+        </div>
       </div>
     </div>
   );
