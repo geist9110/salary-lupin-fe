3a38eecd4e9763f19e6d1f661a3628c7c1f5cd9d
geist9110 <geist9110@naver.com>
2025-04-15 12:39:30 +0900

    refactor: card의 썸네일 alt 부분을 '타이틀 thumbnail' 형식으로 변경

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,7 +5,7 @@ import CardProps from '@/types/card.ts';
 function Card({ thumbnail, title, description, link }: Readonly<CardProps>) {
   return (
     <Link className={styles.cardContainer} to={link}>
-      <img className={styles.image} src={thumbnail} alt="thumbnail" />
+      <img className={styles.image} src={thumbnail} alt={`${title} thumbnail`} />
       <div className={styles.textContainer}>
         <h3>{title}</h3>
         <p className={styles.description}>{description}</p>
