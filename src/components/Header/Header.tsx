8b84b2ca7ba413956965cbc73a151c0f6fff5141
geist9110 <geist9110@naver.com>
2025-04-15 13:03:03 +0900

    feat: 헤더의 로고 부분을 클릭시 홈페이지로 라우팅되는 기능 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import styles from './Header.module.css';
 import logo from '@/assets/common/logo.svg';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <img className={styles.logo} src={logo} alt="logo" />
+      <Link to={'/'}>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </Link>
     </header>
   );
 }
